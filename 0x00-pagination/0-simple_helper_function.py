8a8b28c6c3db3345d636ef3815d866b110d5632c
AyoubMotei <125615908+AyoubMotei@users.noreply.github.com>
2024-01-31 17:03:15 +0100

    Delete 0x00-pagination directory

--- 0x00-pagination/0-simple_helper_function.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from typing import Tuple
-
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    nextPageStartIndex = page * page_size
-    return nextPageStartIndex - page_size, nextPageStartIndex
